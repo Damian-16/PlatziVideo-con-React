0baf6a18abb1c3ad348869e7bc70095cfede135e
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-04-22 15:49:19 -0400

    se añadio static y se utlizo logo y userIcon como logos importados en header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss'
 
+import logo from  '../assets/static/logo-platzi-video-BW2.png';//aca estoy trayendo directamente la imagen de logo para pegarlo en el src de mas abajito
+
+import userIcon from '../assets/static/user-icon.png';
+
+
+
+
+
+
 const Header = () =>(
     <header className="header">
-    <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video"/>
+    <img className="header__img" src={logo} alt="Platzi Video"/>
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src="../assets/user-icon.png" alt=""/>
+        <img src={userIcon} alt=""/>
         <p>Perfil</p>
       </div>
       <ul>
