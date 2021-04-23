c87e882c4cf418674d56a89d460f7680160467c9
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-04-23 19:30:25 -0400

    se creo una fake api con npm install json-server -g y luego npx json-server --watch initialState.json para montarlo , y se modifico el container para hacer el llamado

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -10,7 +10,18 @@ import '../assets/styles/App.scss';
 
 
 
-const App = () => (
+const App = () => {
+    //al llamar a useState y useEffect encapsulo todo en {} y es return quien llama lo que devolvera
+    const [ videos,setVideos ]= useState([]);//primer elemento videos,la variable que se guarda en el estado y set videos es para actualizar,useState recibe parametros de como va estar inicializado el estado (opuede ser un string array,number etc)
+    useEffect(()=>{//funcion anonima
+        //useEffect trae la api atraer informacion para pasarsela a la funcion setVideos que se encarga de actualizar ala variable videos
+        fetch("http://localhost:3000/initialState")// aca va la api que requerimos
+        .then(response =>response.json())//cuando la api resuelva la peticion ,response se transformara en un objeto que pueda usar en mi aplicacion
+        .then(data =>setVideos(data));//otro llamado que me va a  decir a mi informacion que yo ya tengo se la voy a  pasar a mi funcion llamada setVideos y se la voy a transmitir esa informacion en parametro(data)
+    },[]);//use effect recibe otro parametro que se encarga de estar escuchando alguna propiedad q pueda cambiar y asi volver  a ejecutarse ,si no le pasamos esta propiedad va a  crear un loop infinito, le pasamos ese segundo valor como un array vacio
+    console.log(videos)
+    
+    return(
     <div className="App">
 
      <Header/>
@@ -44,6 +55,7 @@ const App = () => (
      <Footer/>
 
     </div>
-)
+      )
+}
 
 export default App;
