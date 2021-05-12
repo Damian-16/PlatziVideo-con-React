e546c0fe7c3b01a4934eb6d32222b95b20efef36
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-12 19:17:50 -0400

    se agrego register  y su scss, la pagina 404 y un switch en App donde el unicoq ue no tiene path es 404

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+
+
+const NotFound =()=> (
+<h1>No encontrado</h1>
+);
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
-<BrowserRouter>
- <Route exact path="/" component={Home}/>  
- <Route exact path="/login" component={Login}/>
+<BrowserRouter> 
+ <Switch>
+    <Route exact path="/" component={Home}/>  
+    <Route exact path="/login" component={Login}/>
+    <Route exact path="/register" component={Register}/>
+    <Route  component={NotFound}/>
+ </Switch>
+ 
 </BrowserRouter>
 ); //return explicito
 //en la "/" del archivo que busque el componente Home, antes denominado App
