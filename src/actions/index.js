e2ebeabc5839f1c4b453bb861f6a94f464889c4f
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-18 11:25:49 -0400

    se agrego codigo para setFavorite

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,5 @@
 //Actions
-import React from 'react'
+export const setFavorite = payload =({//payload es la informacion que le transimitimos
+    type:'SET_FAVORITE',//nombre de descripcion con la que vamos a identificar lo que estamos haciendo
+    payload,
+});
