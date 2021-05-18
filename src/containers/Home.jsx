3b99913199e4f94aee1484ce79c47cda5df0b971
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-18 11:17:31 -0400

    se agrego store en el provider y se arreglo la app

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,50 +1,36 @@
 import React ,{useState,useEffect}from 'react';
-import Header from '../components/Header';
+import {connect} from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
-import useInitialState from '../hooks/useInitialState';
+
 
 import '../assets/styles/App.scss';
 
 
-const API = 'http://localhost:3000/initialState';
 
 
-const Home = () => {
-    //al llamar a useState y useEffect encapsulo todo en {} y es return quien llama lo que devolvera
-   
-   const initialState = useInitialState(API);
- // asi fue primero antes de hooks const [ videos,setVideos ] = useState({mylist:[] , trends:[] , originals:[]});primer elemento videos,la variable que se guarda en el estado y set videos es para actualizar,useState recibe parametros de como va estar inicializado el estado (opuede ser un string array,number etcetera)
-    //luego a useState se les paso los valores mylist ylos otros Eso es porque JavaScript espera encontrar esos elementos, pero al no encontrarlos truena el código
-   
-   /// esto sale como error en la consola porque era la forma que use antes del hook
-    useEffect(()=>{//funcion anonima
-        //useEffect trae la api atraer informacion para pasarsela a la funcion setVideos que se encarga de actualizar ala variable videos
-        fetch("http://localhost:3000/initialState")// aca va la api que requerimos
-        .then(response =>response.json())//cuando la api resuelva la peticion ,response se transformara en un objeto que pueda usar en mi aplicacion
-        .then(data =>setVideos(data));//otro llamado que me va a  decir a mi informacion que yo ya tengo se la voy a  pasar a mi funcion llamada setVideos y se la voy a transmitir esa informacion en parametro(data)
-    },[]);//use effect recibe otro parametro que se encarga de estar escuchando alguna propiedad q pueda cambiar y asi volver  a ejecutarse ,si no le pasamos esta propiedad va a  crear un loop infinito, le pasamos ese segundo valor como un array vacio
-    // console.log(videos)
+
+const Home = ({myList,trends,originals}) => {
+
     
-    return initialState.length === 0? <h1>Loading ...</h1>: (
+   return (
     <>
 
    
      <Search/>
 
-          { initialState.mylist.length >  0 &&    
+          { myList.length >  0 &&    
                 <Categories title="Mi lista">
                     <Carousel>
-                        <CarouselItem/>
+                       {myList.map(item=> <CarouselItem key={item.id}{...item}/>)}
                     </Carousel>
                 </Categories>
           }   
      <Categories title="Tendencias">
          <Carousel>
-             {initialState.trends.map(item =>  //aca hacemos el llamado al video.trends y utilizamos map que lo que hace es traer uno por uno los valores y trae un item, ese item el que va a iterar
+             {trends.map(item =>  //aca hacemos el llamado al video.trends y utilizamos map que lo que hace es traer uno por uno los valores y trae un item, ese item el que va a iterar
              <CarouselItem key={item.id} {...item}/>
              //key lo identifica y luego lo pasamos todos los elementos que contenga item, desestructurandolo {...item}
              )}
@@ -52,7 +38,7 @@ const Home = () => {
      </Categories>
      <Categories title="Originales de PlatziVideo">
          <Carousel>
-             {initialState.originals.map(item =>
+             {originals.map(item =>
               <CarouselItem key={item.id}{...item}/>
              )}
              
@@ -61,9 +47,22 @@ const Home = () => {
          </Carousel>
      </Categories>
     
-      {/* se borro header y footer y volvemos un fragment el div de classnName "Home", que es el mismo de App */}
-    </>
-      )
+     
+    </>)
+       /* se borro header y footer y volvemos un fragment el div de classnName "Home", que es el mismo de App */
 }
 
-export default Home;
+const mapStateToProps = state =>{
+    return {
+        myList: state.myList,
+        trends: state.trends,
+        originals:state.originals,
+    };
+}
+
+
+
+
+// export default Home;
+// export default connect(props,actions)(Home);
+export default connect(mapStateToProps,null)(Home);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,6 @@
 //Reducers
-import React from 'react'
+
+const reducer = (state,action) =>{
+    return state;
+}
+export default reducer;
