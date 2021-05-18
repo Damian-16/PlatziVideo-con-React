bcdd6f16d7e0a59b97b7ae00f7a4122a6f3c8beb
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-18 18:42:03 -0400

    se agrego un props en login paara poder enviar el formulario y simular el inicio de sesion

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,7 +12,12 @@ const reducer = (state,action) =>{
             return {
                 ...state,
                 myList:state.myList.filter(items => items.id !== action.payload)
-            }     
+            }
+        case 'LOGIN_REQUEST':
+            return {
+                ...state,
+                user:action.payload,
+            }         
         default:
             return state;
     }
